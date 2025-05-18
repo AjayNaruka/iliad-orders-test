0d8215fcf61faef53088aa2ab95cbd2f215c96c1
Ajay Naruka <naruka.ajay1997@gmail.com>
2025-05-18 22:05:42 +0200

    [BE] Added JWT Auth

--- backend/src/Controller/ApiController.php
+++ backend/src/Controller/ApiController.php
@@ -3,15 +3,17 @@
 namespace App\Controller;
 
 use App\Entity\Product;
+use App\Entity\User;
 use App\Repository\ProductRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 class ApiController extends AbstractController
 {
-    #[Route('/v1/store-product', methods: ['POST'])]
+    #[Route('/api/v1/store-product', methods: ['POST'])]
     public function storeProduct(EntityManagerInterface $entityManager): Response
     {
         $data = array();
@@ -32,7 +34,7 @@ class ApiController extends AbstractController
         return $this->json("FINE STORE PRODUCT");
     }
 
-    #[Route('/v1/index-product', methods: ['GET'])]
+    #[Route('/api/v1/index-product', methods: ['GET'])]
     public function indexProduct(ProductRepository $pr): Response
     {
         $product = $pr
@@ -42,4 +44,20 @@ class ApiController extends AbstractController
 
         return $this->json("PRICE: " . $product->getPrice());
     }
+
+    #[Route('/api/v1/create-user', methods: ['POST'])]
+    public function createUser(UserPasswordHasherInterface $hasher, EntityManagerInterface $em): Response
+    {
+        $user = new User();
+        $user->setEmail("fapuasn@gmail.com");
+        $user->setRoles(['ROLE_USER']); 
+        $user->setPassword($hasher->hashPassword($user, 'password123'));
+
+        $em->persist($user);
+        $em->flush();
+
+        return new Response('User created');
+    }
+
+
 }
